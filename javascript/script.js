efc5dec7cb71212a35dc5de6e54779cb1a7c742a
amineennaciri <spitlight.enn@gmail.com>
2022-12-08 17:06:38 +0100

    Resolved the problem with my books display. my problem was the card parent of the span before modifiying the value of the innerText. This is why I had the 1st card get the values of the book inside the third card. now everything work logically.

--- javascript/script.js
+++ javascript/script.js
@@ -12,8 +12,6 @@ function Book(title,author,pages,read){
 }
 
 const theHobbit = new Book('The Hobbit','J.R.R. Tolkien','295','not read yet')
-/* console.log(theHobbit.info()); */
-// "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 
 const btnAdd = document.querySelector('.btn')
 const btnDisp = document.querySelector('.btnShow')
@@ -22,24 +20,10 @@ const userAuthor = document.querySelector('.bookAuthor')
 const userPages = document.querySelector('.bookPages')
 const userRead = document.querySelector('.bookRead')
 btnAdd.addEventListener('click',addBookToLibrary);
-btnDisp.addEventListener('click'/* , () => {
-    console.log('hello world');})// */,displayLib);
-/* , () => {
-    console.log('hello world');
-    alert(userInput.value)
-    myLibrary[0] = userInput.value;
-    console.log(myLibrary); 
-    return myLibrary;
-  }) */
+btnDisp.addEventListener('click',displayLib);
 
 function addBookToLibrary(e){
-/*     console.log(e)
-    console.log('hello world'); */
-    /* alert(userTitle.value) */
-    //myLibrary[0] = new Book(`${userTitle.value}`,`${userAuthor.value}`,`${userPages.value}`,`${userRead.value}`);
     myLibrary.push( new Book(`${userTitle.value}`,`${userAuthor.value}`,`${userPages.value}`,`${userRead.value}`) );
-/*     console.log(myLibrary[0].info()); */
-    /* console.log(myLibrary[0].info()) */
      for(let i=0;i<=myLibrary.length-1;i++){
         bookLib[i] = myLibrary[i].info();
     }
@@ -50,7 +34,6 @@ function addBookToLibrary(e){
     document.querySelector('.bookRead').value = ''
     return myLibrary;
 }
-
 function displayLib(){
     // below is the auto refresh functionality before displaying the books
     if(document.querySelector('.card')!=null){
@@ -64,20 +47,7 @@ function displayLib(){
             }
         }
     }
-    // display books code
-    /* big test */
-    /**/
-
-    for(let i=0;i<=bookLib.length-1;i++){
-        console.log(bookLib[i]);
-        
-
-/*     if (cardToggle.style.display === "none") {
-        cardToggle.style.display = "block";
-    } else {
-        cardToggle.style.display = "none";
-    } */
-
+    for(let i = 0; i<= bookLib.length-1;i++){
         /* creating a div card with DOM manipulation */
         const divCard = document.createElement('div');
         //divCard.classList.add(`card${i+1}`)
@@ -85,10 +55,12 @@ function displayLib(){
         // select the card we are working on
         //document.querySelector(`.card${i+1}`)
         //document.querySelector('.display').appendChild(document.getElementsByClassName(`card${i+1}`));
-        //working
+        // this is the trick
         document.querySelector('.display').appendChild(divCard);
+        //working
+        //document.querySelector('.display').appendChild(document.getElementsByClassName('card'));
         //test
-        //document.querySelector('.display').appendChild(document.querySelectorAll('.card')[i]);
+        document.querySelector('.display').appendChild(document.querySelectorAll('.card')[i]);
         const hThree = document.createElement('h3');
         hThree.textContent = `Book ${i+1}`
         //document.getElementsByClassName(`card${i+1}`).appendChild(hThree);
@@ -113,7 +85,7 @@ function displayLib(){
         articleTitle.textContent = 'Title:'
         articleTitle.href = '#'
         spanTitle.classList.add('titleVal')
-        spanTitle.innerText = myLibrary[i].title
+        //spanTitle.innerText = myLibrary[i].title
         iL.appendChild(articleTitle);
         iL.appendChild(spanTitle);
         // author
@@ -122,7 +94,7 @@ function displayLib(){
         articleAuthor.textContent = 'Author:'
         articleAuthor.href = '#'
         spanAuthor.classList.add('authorVal')
-        spanAuthor.innerText = myLibrary[i].author
+        //spanAuthor.innerText = myLibrary[i].author
         iL.appendChild(articleAuthor);
         iL.appendChild(spanAuthor);
         // number of pages
@@ -131,7 +103,7 @@ function displayLib(){
         articlePages.textContent = 'Number of pages:'
         articlePages.href = '#'
         spanPages.classList.add('pagesVal')
-        spanPages.innerText = myLibrary[i].pages
+        //spanPages.innerText = myLibrary[i].pages
         iL.appendChild(articlePages);
         iL.appendChild(spanPages);
         // read status
@@ -140,7 +112,7 @@ function displayLib(){
         articleRead.textContent = 'Read Status'
         articleRead.href = '#'
         spanRead.classList.add('readVal')
-        spanRead.innerText = myLibrary[i].read
+        //spanRead.innerText = myLibrary[i].read
         iL.appendChild(articleRead);
         iL.appendChild(spanRead);
         // css styling for our card-content
@@ -157,14 +129,15 @@ function displayLib(){
         document.querySelector(".readVal").style.gridColumn='2/3';
         //
         /* Add the inputs to the span element */
-        document.querySelector('.titleVal').innerText = myLibrary[i].title
-        document.querySelector('.authorVal').innerText = myLibrary[i].author
-        document.querySelector('.pagesVal').innerText = myLibrary[i].pages
-        document.querySelector('.readVal').innerText = myLibrary[i].read
-        /* Show / Hide the div element */
-/*     let cardToggle = document.querySelector(".card"); */
+        //document.querySelector('.titleVal').innerText = myLibrary[i].title
+        document.querySelectorAll('.card')[i].querySelector('.titleVal').innerText = myLibrary[i].title
+        //document.querySelector('.authorVal').innerText = myLibrary[i].author
+        document.querySelectorAll('.card')[i].querySelector('.authorVal').innerText = myLibrary[i].author
+        //document.querySelector('.pagesVal').innerText = myLibrary[i].pages
+        document.querySelectorAll('.card')[i].querySelector('.pagesVal').innerText = myLibrary[i].pages
+        //document.querySelector('.readVal').innerText = myLibrary[i].read
+        document.querySelectorAll('.card')[i].querySelector('.readVal').innerText = myLibrary[i].read
     }
-
     return bookLib;
 }
 
